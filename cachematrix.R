8997fb297837d6fffe0c86cabe80639ecbfbdffd
AlexTR13 <Alexandra.Resende@kantar.com>
2021-08-20 12:36:46 +0100

    getting read to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#makeCacheMatrix function
+#Changes: m to SValue; mean to solve
+makeCacheMatrix <- function(x = matrix(sample(1:50,9),3,3)) {
+  SValue <- NULL
+  set <- function(y) {
+    x <<- y
+    SValue <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) SValue <<- solve
+  getsolve <- function() SValue
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+#cacheSolve function
+#Changes: m to SValue; mean to solve
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  SValue <- x$getsolve()
+  if(!is.null(SValue)) {
+    message("Getting Inversed Matrix")
+    return(SValue)
+  }
+  data <- x$get()
+  SValue <- solve(data, ...)
+  x$setsolve(SValue)
+  SValue
 }
